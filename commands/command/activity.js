6dfc6a9be6a8106e17fd28d733969cff72928b1a
Pachara Chantawong <66879660+yukikwi@users.noreply.github.com>
2021-08-20 22:29:20 +0700

    Merge json to built in js module

--- commands/command/activity.js
+++ commands/command/activity.js
@@ -1,9 +1,31 @@
 'use strict';
-const fs = require('fs');
+const required_args = 1;
+const activityInvite = require('./plugin/activity/ActivityInvite')
 
-const proc = (client, message, args) => {
-    require("discordjs-activity")(client)
-    const Activitys = JSON.parse(fs.readFileSync('./config/activity.json'));
+const Activitys = [
+  {
+      "name": "yt",
+      "app_id": "755600276941176913"
+  },
+  {
+      "name": "poker",
+      "app_id": "755827207812677713"
+  },
+  {
+      "name": "betrayal",
+      "app_id": "773336526917861400"
+  },
+  {
+      "name": "fishing",
+      "app_id": "814288819477020702"
+  },
+  {
+      "name": "chess",
+      "app_id": "832012774040141894"
+  }
+]
+
+const proc = async (client, message, args) => {
 
     // Find matching
     const Activity = Activitys.find(item => {
@@ -11,14 +33,33 @@ const proc = (client, message, args) => {
     })
 
     // Find voice channel
-    let subscription = subscriptions.get(interaction.guildId);
-    if (!subscription) {
-        message.channel.send("Join voice first.");
+    if (message.member.voice.channelId === null) {
+      return message.channel.send("Join voice first.");
     }
     else{
-
+        let VoiceChannel = client.channels.cache.get(message.member.voice.channelId)
+        let Invite = await activityInvite(VoiceChannel, Activity.app_id)
+        if(Invite.code !== 50013){
+          return message.channel.send("https://discord.com/invite/"+Invite.code)
+        }
+        else{
+          return message.channel.send("Internal error")
+        }
     }
-    let VoiceChannel = client.channels.cache.get("Voice Channel ID")
-    let Invite = await VoiceChannel.activityInvite(Activity.app_id)
-    message.channel.send("https://discord.com/invite/"+Invite.code)
+}
+
+const help = (message) => {
+  return message.channel.send("Support Activity: \n \
+  - Youtube: yt \n \
+  - Poker: poker \n \
+  - Betrayal: betrayal \n \
+  - Fishing: fishing \n \
+  - Chess: chess \
+  ")
+}
+
+module.exports = {
+    required_args,
+    proc,
+    help
 }
