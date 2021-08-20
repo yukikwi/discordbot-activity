2d4d2197f08d7defee14759aca226d460118dd11
Pachara Chantawong <66879660+yukikwi@users.noreply.github.com>
2021-08-20 22:32:34 +0700

    Initial help command

---
+++ commands/command/help.js
@@ -0,0 +1,16 @@
+const required_args = 1;
+
+const proc = (client, message, args) => {
+  const { help } = require('./'+args[0])
+  return help(message)
+}
+
+const help = (message) => {
+  return message.channel.send("Help is help")
+}
+
+module.exports = {
+  required_args,
+  proc,
+  help
+}
