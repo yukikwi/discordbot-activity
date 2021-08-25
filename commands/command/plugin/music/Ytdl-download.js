9eb296ad804d7ca0d6d8198ea61e926db51da107
Pachara Chantawong <66879660+yukikwi@users.noreply.github.com>
2021-08-25 19:09:39 +0700

    Direct ytdl to createAudioResource if don't enable eq

--- commands/command/plugin/music/Ytdl-download.js
+++ commands/command/plugin/music/Ytdl-download.js
@@ -4,24 +4,28 @@ const ytdl = require("ytdl-core");
 const prism = require('prism-media');
 
 module.exports = (url, guild_id) => {
-    let FFMPEG_args = [
-        '-analyzeduration', '0',
-        '-loglevel', '0',
-        '-f', 's16le',
-        '-ar', '48000',
-        '-ac', '2',
-    ]
-    if(getBass(guild_id) === true){
-        console.log('Bass add to pipe')
-        FFMPEG_args.push('-af')
-        FFMPEG_args.push('bass=g=3:f=110:w=0.6')
-    }
-    const transcoder = new prism.FFmpeg({
-        args: FFMPEG_args
-    });
     const stream = ytdl(url, {
         filter: 'audioonly',
         quality: 'highestaudio'
-    }).pipe(transcoder).pipe(new prism.opus.Encoder({ rate: 48000, channels: 2, frameSize: 960 }))
+    })
+    let inputType = StreamType.Arbitrary
+    
+    if(getBass(guild_id) === true){
+        const FFMPEG_args = [
+            '-analyzeduration', '0',
+            '-loglevel', '0',
+            '-f', 's16le',
+            '-ar', '48000',
+            '-ac', '2',
+            '-af', 'bass=g=3:f=110:w=0.6'
+        ]
+        const transcoder = new prism.FFmpeg({
+            args: FFMPEG_args
+        });
+        stream.pipe(transcoder).pipe(new prism.opus.Encoder({ rate: 48000, channels: 2, frameSize: 960 }))
+        inputType = StreamType.Opus
+    }
+    
+    
     return createAudioResource(stream, { inputType: StreamType.Opus })
 }
