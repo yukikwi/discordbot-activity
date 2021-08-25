ea338a69a3c0c2b1faa9d89c06994e5bd87fa7f0
Pachara Chantawong <66879660+yukikwi@users.noreply.github.com>
2021-08-25 18:48:53 +0700

    Fix ffmpeg mix two song on stop then play

--- commands/command/plugin/music/Ytdl-download.js
+++ commands/command/plugin/music/Ytdl-download.js
@@ -2,19 +2,17 @@ const { createAudioResource, StreamType } = require('@discordjs/voice');
 const ytdl = require("ytdl-core");
 const prism = require('prism-media');
 
-const transcoder = new prism.FFmpeg({
-    args: [
-      '-analyzeduration', '0',
-      '-loglevel', '0',
-      '-f', 's16le',
-      '-ar', '48000',
-      '-ac', '2',
-      '-af', "bass=g=3:f=110:w=0.6",
-    ]
-});
-
-
 module.exports = (url) => {
+    const transcoder = new prism.FFmpeg({
+        args: [
+        '-analyzeduration', '0',
+        '-loglevel', '0',
+        '-f', 's16le',
+        '-ar', '48000',
+        '-ac', '2',
+        '-af', "bass=g=3:f=110:w=0.6",
+        ]
+    });
     const stream = ytdl(url, {
         filter: 'audioonly',
         type: 'opus',
