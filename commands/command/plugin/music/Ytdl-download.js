0464fe6b9e092c88b93afc7af7b6454f5731e4e6
Pachara Chantawong <66879660+yukikwi@users.noreply.github.com>
2021-08-25 18:42:14 +0700

    Use prism-media and ffmpeg to eq
    Thanks config from https://lists.ffmpeg.org/pipermail/ffmpeg-user/2013-February/013396.html

--- commands/command/plugin/music/Ytdl-download.js
+++ commands/command/plugin/music/Ytdl-download.js
@@ -1,11 +1,24 @@
 const { createAudioResource, StreamType } = require('@discordjs/voice');
 const ytdl = require("ytdl-core");
+const prism = require('prism-media');
+
+const transcoder = new prism.FFmpeg({
+    args: [
+      '-analyzeduration', '0',
+      '-loglevel', '0',
+      '-f', 's16le',
+      '-ar', '48000',
+      '-ac', '2',
+      '-af', "bass=g=3:f=110:w=0.6",
+    ]
+});
+
 
 module.exports = (url) => {
     const stream = ytdl(url, {
         filter: 'audioonly',
         type: 'opus',
         quality: 'highestaudio'
-    });
-    return createAudioResource(stream, { inputType: StreamType.Arbitrary })
+    }).pipe(transcoder).pipe(new prism.opus.Encoder({ rate: 48000, channels: 2, frameSize: 960 }))
+    return createAudioResource(stream, { inputType: StreamType.Opus })
 }
