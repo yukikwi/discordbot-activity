d14200fc73c3cbe7d93e498ef06efef56fa50d42
Pachara Chantawong <66879660+yukikwi@users.noreply.github.com>
2021-08-31 22:38:15 +0700

    [Music2] Pick bass eq from music

--- commands/command/plugin/music2/Eq.js
+++ commands/command/plugin/music2/Eq.js
@@ -1,8 +1,10 @@
 const { Player, AudioFilters } = require("discord-player");
 const Store = require('./Store')
 
+AudioFilters.define("capybass", "firequalizer=gain_entry='entry(31,3);entry(62,1.5);entry(125,1);entry(500,-0.5);entry(1000,-0.5):delay=0.1:fixed=on:zero_phase=on'");
+
 const eqList = [
-    'bassboost_low', 'bassboost', 'bassboost_high', "8D", 'vaporwave', 'nightcore', 'phaser', 'tremolo', 'vibrato', 'reverse', 'treble', 'normalizer',
+    'capybass', 'bassboost_low', 'bassboost', 'bassboost_high', "8D", 'vaporwave', 'nightcore', 'phaser', 'tremolo', 'vibrato', 'reverse', 'treble', 'normalizer',
     'normalizer2', 'surrounding', 'pulsator', 'subboost', 'karaoke', 'flanger', 'gate', 'haas', 'mcompand', 'mono', 'mstlr', 'mstrr', 'compressor',
     'expander', 'softlimiter', 'chorus', 'chorus2d', 'chorus3d', 'fadein', 'dim', 'earrape', 'off'
 ]
